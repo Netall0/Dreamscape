55485e7f72565225aa54c026c886299790bd32c8
Netall0 <113532176+Netall0@users.noreply.github.com>
2025-11-23 19:25:27 +0700

    chore,fix(app): app_scope,app_widget,runner fix

--- lib/feature/app/logic/runner.dart
+++ lib/feature/app/logic/runner.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
 import 'dart:ui';
 
+import 'package:dreamscape/feature/app/widget/app_scope.dart';
+import 'package:dreamscape/feature/initialization/logic/composition_root.dart';
 import 'package:flutter/widgets.dart';
 import 'package:dreamscape/core/config/app_config.dart';
 import 'package:dreamscape/core/util/logger.dart';
 import 'package:dreamscape/core/util/timer.dart';
-import 'package:dreamscape/feature/app/widget/app_widget.dart';
 
 final class AppRunner with LoggerMixin, AppTimerMixin {
   Future<void> run(AppEnv env) async {
@@ -14,24 +15,20 @@ final class AppRunner with LoggerMixin, AppTimerMixin {
         final bindings = WidgetsFlutterBinding.ensureInitialized()
           ..deferFirstFrame();
 
-
-        
         _initErrorHandler();
-
 
         try {
           logOnProgress('Старт приложения');
 
-        
-          runApp(App());
+          final compositionRoot = await CompositionRoot().compose();
 
-          logOnComplete('AppWidget');
+          runApp(AppScope(dependContainer: compositionRoot.dependModel));
+
+          logOnComplete('runner');
         } on Object catch (e, st) {
           logError('Ошибка в AppRunner', e, st);
         } finally {
-          bindings.addPostFrameCallback((_) {
-            bindings.allowFirstFrame();
-          });
+          bindings.allowFirstFrame();
         }
       },
       (error, stackTrace) {
@@ -40,7 +37,6 @@ final class AppRunner with LoggerMixin, AppTimerMixin {
     );
   }
 }
-
 
 void _initErrorHandler() {
   PlatformDispatcher.instance.onError = (error, stack) {
