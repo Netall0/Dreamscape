d8a77d1e05c5168ca8d01c34e4f9352a5378319f
Netall0 <113532176+Netall0@users.noreply.github.com>
2025-12-19 23:40:57 +0700

    feat(router): adding routing flow

---
+++ lib/core/router/router.dart
@@ -0,0 +1,110 @@
+import 'package:dreamscape/features/app/widget/root_screen.dart';
+import 'package:dreamscape/features/home/widget/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:dreamscape/features/home/widget/sleep_screen.dart';
+
+part 'router.g.dart';
+
+@TypedStatefulShellRoute<RootRouteData>(
+  branches: <TypedStatefulShellBranch<StatefulShellBranchData>>[
+    //home
+    TypedStatefulShellBranch<HomeShellBranchData>(
+      routes: <TypedRoute<RouteData>>[
+        TypedGoRoute<HomeRouteDate>(
+          path: '/home',
+          routes: [TypedGoRoute<SleepScreenData>(path: '/home/sleep')],
+        ),
+      ],
+    ),
+
+    // stats
+    TypedStatefulShellBranch<StatsShellBranchData>(
+      routes: <TypedRoute<RouteData>>[TypedGoRoute<StatsData>(path: '/stats')],
+    ),
+
+    //profile
+    TypedStatefulShellBranch<StatefulShellBranchData>(
+      routes: <TypedRoute<RouteData>>[
+        TypedGoRoute<ProfileScreenData>(path: '/profile'),
+      ],
+    ),
+  ],
+)
+class RootRouteData extends StatefulShellRouteData {
+  @override
+  Widget builder(
+    BuildContext context,
+    GoRouterState state,
+    StatefulNavigationShell navigationShell,
+  ) {
+    return RootScreen(navigationShell);
+  }
+}
+
+//notificaiotions branch
+
+//home branch
+
+class HomeShellBranchData extends StatefulShellBranchData {}
+
+class HomeRouteDate extends GoRouteData with $HomeRouteDate {
+  const HomeRouteDate();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return HomeScreen();
+  }
+}
+
+class SleepScreenData extends GoRouteData with $SleepScreenData {
+  const SleepScreenData();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return SleepScreen();
+  }
+}
+
+//statsBracnh
+
+class StatsShellBranchData extends StatefulShellBranchData {}
+
+class StatsData extends GoRouteData with $StatsData {
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return StatsScreen();
+  }
+}
+
+// ProfileBranch
+
+class ProfileShellBranchData extends StatefulShellBranchData {}
+
+class ProfileScreenData extends GoRouteData with $ProfileScreenData {
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    // TODO: implement build
+    return ProfileScreen();
+  }
+}
+
+//TODO Delete when  finish the corresponding features
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.green);
+  }
+}
+
+class StatsScreen extends StatelessWidget {
+  const StatsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.redAccent);
+  }
+}
---
+++ lib/core/router/router.g.dart
@@ -0,0 +1,128 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'router.dart';
+
+// **************************************************************************
+// GoRouterGenerator
+// **************************************************************************
+
+List<RouteBase> get $appRoutes => [$rootRouteData];
+
+RouteBase get $rootRouteData => StatefulShellRouteData.$route(
+  factory: $RootRouteDataExtension._fromState,
+  branches: [
+    StatefulShellBranchData.$branch(
+      routes: [
+        GoRouteData.$route(
+          path: '/home',
+          factory: $HomeRouteDate._fromState,
+          routes: [
+            GoRouteData.$route(
+              path: '/home/sleep',
+              factory: $SleepScreenData._fromState,
+            ),
+          ],
+        ),
+      ],
+    ),
+    StatefulShellBranchData.$branch(
+      routes: [
+        GoRouteData.$route(path: '/stats', factory: $StatsData._fromState),
+      ],
+    ),
+    StatefulShellBranchData.$branch(
+      routes: [
+        GoRouteData.$route(
+          path: '/profile',
+          factory: $ProfileScreenData._fromState,
+        ),
+      ],
+    ),
+  ],
+);
+
+extension $RootRouteDataExtension on RootRouteData {
+  static RootRouteData _fromState(GoRouterState state) => RootRouteData();
+}
+
+mixin $HomeRouteDate on GoRouteData {
+  static HomeRouteDate _fromState(GoRouterState state) => const HomeRouteDate();
+
+  @override
+  String get location => GoRouteData.$location('/home');
+
+  @override
+  void go(BuildContext context) => context.go(location);
+
+  @override
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  @override
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  @override
+  void replace(BuildContext context) => context.replace(location);
+}
+
+mixin $SleepScreenData on GoRouteData {
+  static SleepScreenData _fromState(GoRouterState state) =>
+      const SleepScreenData();
+
+  @override
+  String get location => GoRouteData.$location('/home/sleep');
+
+  @override
+  void go(BuildContext context) => context.go(location);
+
+  @override
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  @override
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  @override
+  void replace(BuildContext context) => context.replace(location);
+}
+
+mixin $StatsData on GoRouteData {
+  static StatsData _fromState(GoRouterState state) => StatsData();
+
+  @override
+  String get location => GoRouteData.$location('/stats');
+
+  @override
+  void go(BuildContext context) => context.go(location);
+
+  @override
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  @override
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  @override
+  void replace(BuildContext context) => context.replace(location);
+}
+
+mixin $ProfileScreenData on GoRouteData {
+  static ProfileScreenData _fromState(GoRouterState state) =>
+      ProfileScreenData();
+
+  @override
+  String get location => GoRouteData.$location('/profile');
+
+  @override
+  void go(BuildContext context) => context.go(location);
+
+  @override
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  @override
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  @override
+  void replace(BuildContext context) => context.replace(location);
+}
