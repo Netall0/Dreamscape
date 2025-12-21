85884f990202809b03b8b29c9ec78463113c6b36
Netall0 <113532176+Netall0@users.noreply.github.com>
2025-12-21 18:01:50 +0700

    yagni notifications_service

--- lib/core/services/audio/i_audio_service.dart
+++
@@ -1,4 +0,0 @@
-import 'package:just_audio/just_audio.dart';
-
-final class IAudioService {
-}
--- lib/core/services/notifications/i_notifications_sender.dart
+++
@@ -1,7 +0,0 @@
-abstract interface class INotificationsSender {
-  Future<void> showNotification(
-    final int id,
-    final String name,
-    final String description,
-  );
-}
